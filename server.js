ecc61341170d29785dca67093eac2e90d70aae5c
Federico <federicomatias.aguero1987@gmail.com>
2019-04-15 11:27:04 -0300

    segundo commit

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
 
+hbs = require('hbs');
+require('./hbs/helpers/helpers');
+
+const port = process.env.Port || 3000;
 //Me permite mostrar todo el HTML que esta en la carpeta publica
 //No se debe colocar con peticiones app.get ya que va depender del
 //motor que la interprete para mostrar.
 app.use(express.static(__dirname + '/public'))
 
+hbs.registerPartials(__dirname + '/views/parciales');
+//Express HBS
+app.set('view engine', 'hbs');
 //Configura una solicitud get cuando el path sea /
-//app.get('/', (req, resp) => {
-//    let salida = {
-//        nombre: 'federico',
-//        edad: 32,
-//        url: req.url
-//    };
-//    resp.send(salida);
 
 
-//});
+
+app.get('/', (req, resp) => {
+
+    resp.render('home', {
+        nombre: 'fede',
+        anio: new Date().getFullYear()
+    });
+});
+app.get('/about', (req, resp) => {
+
+    resp.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
 //se Configura por cada peticion get dependiendo como empiza
 //la anterior es con / y la actual /data
 //app.get('/data', (req, resp) => {
@@ -24,6 +37,6 @@ app.use(express.static(__dirname + '/public'))
 //});
 
 
-app.listen(3000, () => {
-    console.log('Escuchando en el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${{port}}`);
 });
